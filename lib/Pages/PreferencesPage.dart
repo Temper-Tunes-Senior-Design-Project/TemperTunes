06c535cb74242ebc78af95d99a71212b363d2828
Dalton Leight <dtleight@gmail.com>
2023-02-19 17:19:59 -0500

    Fixed file naming

--- lib/Pages/PreferencesPage.dart
+++ lib/Pages/PreferencesPage.dart
@@ -23,7 +23,7 @@ class LargeScreen extends StatelessWidget {
         height: height,
         decoration:BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("assets/LoginPageLarge.png"), fit: BoxFit.cover),
+              image: AssetImage("assets/loginPageLarge.png"), fit: BoxFit.cover),
         ),
           ///back arrow
           child: Column(
