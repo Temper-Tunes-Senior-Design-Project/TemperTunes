4052f70d98a6281b29f9e6e7c36244540267df11
Dalton Leight <dtleight@gmail.com>
2023-03-05 12:21:01 -0500

    Finished android spotify integration

---
+++ lib/Utilities/SpotifyRouter.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:spotify_sdk/spotify_sdk.dart';
+
+class SpotifyRouter
+{
+  Future<void> getToken() async{
+    var accessToken = await SpotifySdk.getAccessToken(clientId: dotenv.env['SPOTIFY_CLIENT_ID']??"", redirectUrl:  dotenv.env['SPOTIFY_REDIRECT_URI']??"", scope: "app-remote-control,user-modify-playback-state,playlist-read-private");
+    print(accessToken);
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,22 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:mood_swing/Utilities/SpotifyRouter.dart';
 import 'Pages/HomePage.dart';
 import 'Pages/LandingPage.dart';
 import 'Widgets/MockNavigator.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load(fileName: ".env");
+  print(dotenv.env['SPOTIFY_CLIENT_ID']);
+  SpotifyRouter().getToken();
 
   //Catch Flutter framework errors
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
