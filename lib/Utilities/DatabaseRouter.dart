e904f852bb6678c9db39f57c5bad4400f8d72c77
Dalton Leight <dtleight@gmail.com>
2023-03-06 23:59:00 -0500

    Added image uploading

--- lib/Utilities/DatabaseRouter.dart
+++ lib/Utilities/DatabaseRouter.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:mood_swing/Objects/FileTypes.dart';
 
 class DatabaseRouter {
   String uid = FirebaseAuth.instance.currentUser?.uid ?? "-1";
@@ -14,5 +20,16 @@ class DatabaseRouter {
         'statistics' : {},
       },
     );
+  }
+
+  Future<void> uploadFile(XFile? file, FileType encodingFormat) async
+  {
+    if(file != null) {
+      String path = "Processing Data/file." + encodingFormat.getPostfix();
+      TaskSnapshot snap = await FirebaseStorage.instance.ref(path).putData(await file.readAsBytes(),SettableMetadata(
+        contentType: encodingFormat.getEncoding(),
+      ));
+      print(await snap.ref.getDownloadURL());
+    }
   }
 }
