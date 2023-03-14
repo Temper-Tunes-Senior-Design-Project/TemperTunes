eec98a4b41cdc078207208ffd17a33b56ab9cffb
Dalton Leight <dtleight@gmail.com>
2023-03-14 17:37:12 -0400

    Fixed static analysis

--- lib/Widgets/SidebarSmall.dart
+++ lib/Widgets/SidebarSmall.dart
@@ -1,4 +1,3 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:mood_swing/Pages/CameraPage.dart';
 import 'package:mood_swing/Pages/PreferencesPage.dart';
