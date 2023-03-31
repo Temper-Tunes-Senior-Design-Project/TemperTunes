968390a074d117218fdc3f6ee3f9a8be9f473cfd
Min-Yi <63892175+Min-Yi@users.noreply.github.com>
2023-03-31 15:57:27 -0400

    Updated files

--- lib/Widgets/ClassPopup.dart
+++ lib/Widgets/ClassPopup.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
-//
-// class ClassPopup {
-//   AwesomeDialog getDialogue(context) {
-//     return AwesomeDialog(
-//       context: context,
-//       animType: AnimType.SCALE,
-//       dialogType: DialogType.INFO,
-//       body: Center(
-//         child: Text(
-//           'If the body is specified, then title and description will be ignored, this allows to further customize the dialogue.',
-//           style: TextStyle(fontStyle: FontStyle.italic),
-//         ),
-//       ),
-//       title: 'This is Ignored',
-//       desc: 'This is also Ignored',
-//       btnOkOnPress: () {},
-//     );
-//   }
-// }
+
+class ClassPopup {
+  AwesomeDialog getDialogue(context) {
+    return AwesomeDialog(
+      context: context,
+      animType: AnimType.SCALE,
+      dialogType: DialogType.INFO,
+      body: Center(
+        child: Text(
+          'If the body is specified, then title and description will be ignored, this allows to further customize the dialogue.',
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+      ),
+      title: 'This is Ignored',
+      desc: 'This is also Ignored',
+      btnOkOnPress: () {},
+    );
+  }
+}
 //
 // import 'dart:ui';
 // import 'package:flutter/cupertino.dart';
