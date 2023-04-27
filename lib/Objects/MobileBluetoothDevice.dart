529cdcbb2b8fefff73493d9adceebef5c9fc5d5b
Dalton Leight <dtleight@gmail.com>
2023-04-26 23:52:18 -0400

    Intermediate commit

---
+++ lib/Objects/MobileBluetoothDevice.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+class MobileBluetoothDevice
+{
+  String name;
+  DiscoveredDevice device;
+
+  MobileBluetoothDevice(this.name, this.device);
+
+}
--- lib/Utilities/BluetoothRouter.dart
+++ lib/Utilities/BluetoothRouter.dart
@@ -9,4 +9,28 @@ class BluetoothRouter {
         withServices: [],
         scanMode: ScanMode.lowLatency).map((device) => device.name);
   }
+
+  void writeToDevice(DiscoveredDevice device) async {
+    // print(device);
+    // await device?.connect();
+    // print(await device?.discoverServices());
+    // device?.services.listen((event) async {
+    //   BluetoothCharacteristic? char = (await event[0].getCharacteristics())[0];
+    //   char.writeValueWithoutResponse(Uint8List.fromList(utf8.encode("on")));
+    //   print("Written");
+    // });
+  }
+
+  void readFromDevice() async {
+    // print(device.toString());
+    // await device?.connect();
+    // print(await device?.discoverServices());
+    // device?.services.listen((event) async {
+    //   BluetoothCharacteristic? char = (await event[0].getCharacteristics())?[0];
+    //   print(await char?.readValue());
+    //   //ByteData? b = await char?.readValue();
+    //   print("Read");
+    //   // print(b?.buffer.asUint8List());
+    // });
+  }
 }
